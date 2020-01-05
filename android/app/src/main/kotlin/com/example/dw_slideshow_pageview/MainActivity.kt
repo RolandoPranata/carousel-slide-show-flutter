34d145e56ac0530b5580f11cbc81d332894bea20
Anugrah Sandi <makassarnetwork06@gmail.com>
2020-01-05 08:14:23 +0700

    Mengenal Widget Flutter #8: Membuat Carousel Slide Show Dengan List View & Page View

---
+++ android/app/src/main/kotlin/com/example/dw_slideshow_pageview/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.dw_slideshow_pageview
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
